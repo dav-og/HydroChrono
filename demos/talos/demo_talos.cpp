fc64c71635d6a300c12552899dcb8fb660c7069f
David Ogden <david.ogden.nrel@outlook.com>
2023-06-07 22:07:32 +0100

    single body model

--- demos/talos/demo_talos.cpp
+++ demos/talos/demo_talos.cpp
@@ -94,10 +94,12 @@ int main(int argc, char* argv[]) {
     // some io/viz options
     bool visualization_on = true;
     bool profiling_on     = true;
-    bool save_data_on     = false;
+    bool save_data_on     = true;
     std::vector<double> time_vector;
-    std::vector<double> hull_pitch;
     std::vector<double> hull_surge;
+    std::vector<double> hull_heave;
+    std::vector<double> hull_pitch;
+
 
     // set up hull from a mesh
     std::cout << "Attempting to open mesh file: " << hull_mesh_fname << std::endl;
@@ -120,135 +122,137 @@ int main(int argc, char* argv[]) {
     // define the hull's initial conditions
     auto cg = ChVector<>(0.0, 0.0, 0.0);
     // offset used for heave/surge decay test
-    auto offset = ChVector<>(0.0, 0.0, 0.0);
+    auto offset = ChVector<>(0.0, 0.0, 3.0);
     hull->SetPos(cg + offset);
     // Use for pitch decay test
     double ang_rad = 0.0; //-3.95 * CH_C_PI / 180.0;
     hull->SetRot(Q_from_AngAxis(ang_rad, VECT_Y));
-    hull->SetMass(3.85e6);
-    hull->SetInertiaXX(ChVector<>(3.85e6, 3.85e6, 3.85e6));
+    hull->SetMass(3747762);
+    hull->SetInertiaXX(ChVector<>(2.376e8, 2.376e8, 2.376e8));
 
-    // internal reaction mass
-    auto irm = chrono_types::make_shared<ChBodyEasySphere>(4.0, 1000.0, true, false);
-    system.Add(irm);
-    //mrigidBody->SetRot(randrot);
-    irm->SetPos(ChVector<>(0.0, 0.0, -5.7));
+    //// internal reaction mass
+    //auto irm = chrono_types::make_shared<ChBodyEasySphere>(4.0, 1000.0, true, false);
+    //system.Add(irm);
+    ////mrigidBody->SetRot(randrot);
+    //irm->SetPos(ChVector<>(0.0, 0.0, -5.7));
 
-    // PTO parameters
-    double pto_length    = 5.0;
-    double pto_stiffness = 3.0E+05;
-    double pto_damping   = 1.5E+05;
-    double pto_radius    = 0.75;
-    int pto_resolution   = 80;
-    int pto_spring_turns = 10;
+    //// PTO parameters
+    //double pto_length    = 5.0;
+    //double pto_stiffness = 3.0E+05;
+    //double pto_damping   = 1.5E+05;
+    //double pto_radius    = 0.75;
+    //int pto_resolution   = 80;
+    //int pto_spring_turns = 10;
 
-    // spring 1 between hull and irm
-    auto spring_1 = chrono_types::make_shared<ChLinkTSDA>();
-    spring_1->Initialize(hull, irm, true,
-                         ChVector<>(5.000, 0.000, -8.660-5.7),
-                         ChVector<>(2.500, 0.000, -4.330));
-    spring_1->SetRestLength(pto_length);
-    spring_1->SetSpringCoefficient(pto_stiffness);
-    spring_1->SetDampingCoefficient(pto_damping);
-    spring_1->AddVisualShape(chrono_types::make_shared<ChSpringShape>(pto_radius, pto_resolution, pto_spring_turns));
-    system.AddLink(spring_1);
+    //// spring 1 between hull and irm
+    //auto spring_1 = chrono_types::make_shared<ChLinkTSDA>();
+    //spring_1->Initialize(hull, irm, true,
+    //                     ChVector<>(5.000, 0.000, -8.660-5.7),
+    //                     ChVector<>(2.500, 0.000, -4.330));
+    //spring_1->SetRestLength(pto_length);
+    //spring_1->SetSpringCoefficient(pto_stiffness);
+    //spring_1->SetDampingCoefficient(pto_damping);
+    //spring_1->AddVisualShape(chrono_types::make_shared<ChSpringShape>(pto_radius, pto_resolution, pto_spring_turns));
+    //system.AddLink(spring_1);
 
-    // spring 2 between hull and irm
-    auto spring_2 = chrono_types::make_shared<ChLinkTSDA>();
-    spring_2->Initialize(hull, irm, true,
-                         ChVector<>(-2.500, 4.330, -8.660-5.7),
-                         ChVector<>(-1.250, 2.165, -4.330));
-    spring_2->SetRestLength(pto_length);
-    spring_2->SetSpringCoefficient(pto_stiffness);
-    spring_2->SetDampingCoefficient(pto_damping);
-    spring_2->AddVisualShape(chrono_types::make_shared<ChSpringShape>(pto_radius, pto_resolution, pto_spring_turns));
-    system.AddLink(spring_2);
+    //// spring 2 between hull and irm
+    //auto spring_2 = chrono_types::make_shared<ChLinkTSDA>();
+    //spring_2->Initialize(hull, irm, true,
+    //                     ChVector<>(-2.500, 4.330, -8.660-5.7),
+    //                     ChVector<>(-1.250, 2.165, -4.330));
+    //spring_2->SetRestLength(pto_length);
+    //spring_2->SetSpringCoefficient(pto_stiffness);
+    //spring_2->SetDampingCoefficient(pto_damping);
+    //spring_2->AddVisualShape(chrono_types::make_shared<ChSpringShape>(pto_radius, pto_resolution, pto_spring_turns));
+    //system.AddLink(spring_2);
 
-    // spring 3 between hull and irm
-    auto spring_3 = chrono_types::make_shared<ChLinkTSDA>();
-    spring_3->Initialize(hull, irm, true,
-                         ChVector<>(-2.500, -4.330, -8.660-5.7),
-                         ChVector<>(-1.250, -2.165, -4.330));
-    spring_3->SetRestLength(pto_length);
-    spring_3->SetSpringCoefficient(pto_stiffness);
-    spring_3->SetDampingCoefficient(pto_damping);
-    spring_3->AddVisualShape(chrono_types::make_shared<ChSpringShape>(pto_radius, pto_resolution, pto_spring_turns));
-    system.AddLink(spring_3);
+    //// spring 3 between hull and irm
+    //auto spring_3 = chrono_types::make_shared<ChLinkTSDA>();
+    //spring_3->Initialize(hull, irm, true,
+    //                     ChVector<>(-2.500, -4.330, -8.660-5.7),
+    //                     ChVector<>(-1.250, -2.165, -4.330));
+    //spring_3->SetRestLength(pto_length);
+    //spring_3->SetSpringCoefficient(pto_stiffness);
+    //spring_3->SetDampingCoefficient(pto_damping);
+    //spring_3->AddVisualShape(chrono_types::make_shared<ChSpringShape>(pto_radius, pto_resolution, pto_spring_turns));
+    //system.AddLink(spring_3);
 
-    // spring 4 between hull and irm
-    auto spring_4 = chrono_types::make_shared<ChLinkTSDA>();
-    spring_4->Initialize(hull, irm, true,
-                         ChVector<>(5.000, 0.000, 8.660-5.7), 
-                         ChVector<>(2.500, 0.000, 4.330));
-    spring_4->SetRestLength(pto_length);
-    spring_4->SetSpringCoefficient(pto_stiffness);
-    spring_4->SetDampingCoefficient(pto_damping);
-    spring_4->AddVisualShape(chrono_types::make_shared<ChSpringShape>(pto_radius, pto_resolution, pto_spring_turns));
-    system.AddLink(spring_4);
+    //// spring 4 between hull and irm
+    //auto spring_4 = chrono_types::make_shared<ChLinkTSDA>();
+    //spring_4->Initialize(hull, irm, true,
+    //                     ChVector<>(5.000, 0.000, 8.660-5.7), 
+    //                     ChVector<>(2.500, 0.000, 4.330));
+    //spring_4->SetRestLength(pto_length);
+    //spring_4->SetSpringCoefficient(pto_stiffness);
+    //spring_4->SetDampingCoefficient(pto_damping);
+    //spring_4->AddVisualShape(chrono_types::make_shared<ChSpringShape>(pto_radius, pto_resolution, pto_spring_turns));
+    //system.AddLink(spring_4);
 
-    // spring 5 between hull and irm
-    auto spring_5 = chrono_types::make_shared<ChLinkTSDA>();
-    spring_5->Initialize(hull, irm, true,
-                         ChVector<>(-2.500, 4.330, 8.660-5.7),
-                         ChVector<>(-1.250, 2.165, 4.330));
-    spring_5->SetRestLength(pto_length);
-    spring_5->SetSpringCoefficient(pto_stiffness);
-    spring_5->SetDampingCoefficient(pto_damping);
-    spring_5->AddVisualShape(chrono_types::make_shared<ChSpringShape>(pto_radius, pto_resolution, pto_spring_turns));
-    system.AddLink(spring_5);
+    //// spring 5 between hull and irm
+    //auto spring_5 = chrono_types::make_shared<ChLinkTSDA>();
+    //spring_5->Initialize(hull, irm, true,
+    //                     ChVector<>(-2.500, 4.330, 8.660-5.7),
+    //                     ChVector<>(-1.250, 2.165, 4.330));
+    //spring_5->SetRestLength(pto_length);
+    //spring_5->SetSpringCoefficient(pto_stiffness);
+    //spring_5->SetDampingCoefficient(pto_damping);
+    //spring_5->AddVisualShape(chrono_types::make_shared<ChSpringShape>(pto_radius, pto_resolution, pto_spring_turns));
+    //system.AddLink(spring_5);
 
-    // spring 6 between hull and irm
-    auto spring_6 = chrono_types::make_shared<ChLinkTSDA>();
-    spring_6->Initialize(hull, irm, true,
-                         ChVector<>(-2.500, -4.330, 8.660-5.7),
-                         ChVector<>(-1.250, -2.165, 4.330));
-    spring_6->SetRestLength(pto_length);
-    spring_6->SetSpringCoefficient(pto_stiffness);
-    spring_6->SetDampingCoefficient(pto_damping);
-    spring_6->AddVisualShape(chrono_types::make_shared<ChSpringShape>(pto_radius, pto_resolution, pto_spring_turns));
-    system.AddLink(spring_6);
+    //// spring 6 between hull and irm
+    //auto spring_6 = chrono_types::make_shared<ChLinkTSDA>();
+    //spring_6->Initialize(hull, irm, true,
+    //                     ChVector<>(-2.500, -4.330, 8.660-5.7),
+    //                     ChVector<>(-1.250, -2.165, 4.330));
+    //spring_6->SetRestLength(pto_length);
+    //spring_6->SetSpringCoefficient(pto_stiffness);
+    //spring_6->SetDampingCoefficient(pto_damping);
+    //spring_6->AddVisualShape(chrono_types::make_shared<ChSpringShape>(pto_radius, pto_resolution, pto_spring_turns));
+    //system.AddLink(spring_6);
 
-    auto my_hydro_inputs = std::make_shared<IrregularWave>();
-    // my_hydro_inputs->mode                   = WaveMode::irregular;                     // uses regular wave mode
-    my_hydro_inputs->wave_height         = 2.0;
-    my_hydro_inputs->wave_period         = 6.0;
-    my_hydro_inputs->simulation_duration = simulation_duration;
-    my_hydro_inputs->simulation_dt       = timestep;
-    my_hydro_inputs->ramp_duration       = simulation_duration*0.1;
+    //auto my_hydro_inputs = std::make_shared<IrregularWave>();
+    //// my_hydro_inputs->mode                   = WaveMode::irregular;                     // uses regular wave mode
+    //my_hydro_inputs->wave_height         = 2.0;
+    //my_hydro_inputs->wave_period         = 6.0;
+    //my_hydro_inputs->simulation_duration = simulation_duration;
+    //my_hydro_inputs->simulation_dt       = timestep;
+    //my_hydro_inputs->ramp_duration       = simulation_duration*0.1;
+
+    auto default_dont_add_waves = std::make_shared<NoWave>(1);
 
     // set up hydro forces
     std::vector<std::shared_ptr<ChBody>> bodies;
     bodies.push_back(hull);
     TestHydro hydro_forces(bodies, h5_fname);
-    hydro_forces.AddWaves(my_hydro_inputs);
+    hydro_forces.AddWaves(default_dont_add_waves);
 
-    // set up free surface from a mesh
-    auto fse_plane = chrono_types::make_shared<ChBody>();
-    fse_plane->SetPos(ChVector<>(0, 0, 0));
-    fse_plane->SetBodyFixed(true);
-    fse_plane->SetCollide(false);
-    system.AddBody(fse_plane);
+    //// set up free surface from a mesh
+    //auto fse_plane = chrono_types::make_shared<ChBody>();
+    //fse_plane->SetPos(ChVector<>(0, 0, 0));
+    //fse_plane->SetBodyFixed(true);
+    //fse_plane->SetCollide(false);
+    //system.AddBody(fse_plane);
 
-    my_hydro_inputs->SetUpWaveMesh();
-    std::shared_ptr<ChBody> fse_mesh = chrono_types::make_shared<ChBodyEasyMesh>(  //
-        my_hydro_inputs->GetMeshFile(),                                            // file name
-        1000,                                                                      // density
-        false,                                                                     // do not evaluate mass automatically
-        true,                                                                      // create visualization asset
-        false                                                                      // do not collide
-    );
-    fse_mesh->SetMass(1.0);
-    fse_mesh->SetPos_dt(my_hydro_inputs->GetWaveMeshVelocity());
-    system.Add(fse_mesh);
-    auto fse_prismatic = chrono_types::make_shared<ChLinkLockPrismatic>();
-    fse_prismatic->Initialize(fse_plane, fse_mesh, ChCoordsys<>(ChVector<>(1.0, 0.0, 0.0), Q_from_AngY(CH_C_PI_2)));
-    system.AddLink(fse_prismatic);
+    //my_hydro_inputs->SetUpWaveMesh();
+    //std::shared_ptr<ChBody> fse_mesh = chrono_types::make_shared<ChBodyEasyMesh>(  //
+    //    my_hydro_inputs->GetMeshFile(),                                            // file name
+    //    1000,                                                                      // density
+    //    false,                                                                     // do not evaluate mass automatically
+    //    true,                                                                      // create visualization asset
+    //    false                                                                      // do not collide
+    //);
+    //fse_mesh->SetMass(1.0);
+    //fse_mesh->SetPos_dt(my_hydro_inputs->GetWaveMeshVelocity());
+    //system.Add(fse_mesh);
+    //auto fse_prismatic = chrono_types::make_shared<ChLinkLockPrismatic>();
+    //fse_prismatic->Initialize(fse_plane, fse_mesh, ChCoordsys<>(ChVector<>(1.0, 0.0, 0.0), Q_from_AngY(CH_C_PI_2)));
+    //system.AddLink(fse_prismatic);
 
-    // Create a visualization material
-    auto fse_texture = chrono_types::make_shared<ChVisualMaterial>();
-    fse_texture->SetDiffuseColor(ChColor(0.026f, 0.084f, 0.168f));
-    fse_texture->SetOpacity(0.1);
-    fse_mesh->GetVisualShape(0)->SetMaterial(0, fse_texture);
+    //// Create a visualization material
+    //auto fse_texture = chrono_types::make_shared<ChVisualMaterial>();
+    //fse_texture->SetDiffuseColor(ChColor(0.026f, 0.084f, 0.168f));
+    //fse_texture->SetOpacity(0.1);
+    //fse_mesh->GetVisualShape(0)->SetMaterial(0, fse_texture);
 
     // for profiling
     auto start = std::chrono::high_resolution_clock::now();
@@ -259,7 +263,7 @@ int main(int argc, char* argv[]) {
         auto irrlicht_vis = chrono_types::make_shared<ChVisualSystemIrrlicht>();
         irrlicht_vis->AttachSystem(&system);
         irrlicht_vis->SetWindowSize(1280, 720);
-        irrlicht_vis->SetWindowTitle("DeepCwind Verification");
+        irrlicht_vis->SetWindowTitle("TALOS Decay");
         irrlicht_vis->SetCameraVertical(CameraVerticalDir::Z);
         irrlicht_vis->Initialize();
         irrlicht_vis->AddLogo();
@@ -289,6 +293,7 @@ int main(int argc, char* argv[]) {
                 // append data to std vector
                 time_vector.push_back(system.GetChTime());
                 hull_surge.push_back(hull->GetPos().x());
+                hull_heave.push_back(hull->GetPos().z());
                 hull_pitch.push_back(hull->GetRot().Q_to_Euler123().y());
                 // force playback to be real-time
                 realtime_timer.Spin(timestep);
@@ -300,6 +305,7 @@ int main(int argc, char* argv[]) {
             // append data to std vector
             time_vector.push_back(system.GetChTime());
             hull_surge.push_back(hull->GetPos().x());
+            hull_heave.push_back(hull->GetPos().z());
             hull_pitch.push_back(hull->GetRot().Q_to_Euler123().y());
             // step the simulation forwards
             system.DoStepDynamics(timestep);
@@ -326,10 +332,12 @@ int main(int argc, char* argv[]) {
             }
         }
         output_file << std::left << std::setw(10) << "Time (s)" << std::right << std::setw(16) << "Hull Surge (m)"
-                   << std::right << std::setw(16) << "Hull Pitch (radians)" << std::endl;
+                    << std::right << std::setw(16) << "Hull Heave (m)"
+                    << std::right << std::setw(16) << "Hull Pitch (radians)" << std::endl;
         for (int i = 0; i < time_vector.size(); ++i)
             output_file << std::left << std::setw(10) << std::setprecision(2) << std::fixed << time_vector[i]
                         << std::right << std::setw(16) << std::setprecision(8) << std::fixed << hull_surge[i]
+                        << std::right << std::setw(16) << std::setprecision(8) << std::fixed << hull_heave[i]
                         << std::right << std::setw(16) << std::setprecision(8) << std::fixed << hull_pitch[i]
                         << std::endl;
         output_file.close();
